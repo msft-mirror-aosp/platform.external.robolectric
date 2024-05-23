4a7c8ceca0f6ddb278e66ea5f8ecaf164bea60c7
Ang Li <ihcinihsdk@google.com>
2024-05-23 20:02:19 +0000

    Align several shadows with upstream.
    Bug: 341163842
    Test: (Tested in git-main)
        atest -c CtesqueRoboTests
        atest -c SystemUIGoogleRobo(2)RNGTests
    Ignore-AOSP-First: The purpose of this CL is to avoid the merge conflict
        caused from a downstream merge.
    Merged-In: Iebe80a1a0e9378dcd405c73fa954bc74df13574b
    Change-Id: Iebe80a1a0e9378dcd405c73fa954bc74df13574b

--- shadows/framework/src/main/java/org/robolectric/shadows/ShadowNativeMatrix.java
+++ shadows/framework/src/main/java/org/robolectric/shadows/ShadowNativeMatrix.java
@@ -11,7 +11,6 @@ import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
 import org.robolectric.nativeruntime.DefaultNativeRuntimeLoader;
 import org.robolectric.nativeruntime.MatrixNatives;
-// import org.robolectric.shadows.ShadowNativeMatrix.ShadowExtraNatives.Picker;
 import org.robolectric.versioning.AndroidVersions.U;
 import org.robolectric.versioning.AndroidVersions.V;
 
@@ -266,5 +265,22 @@ public class ShadowNativeMatrix extends ShadowMatrix {
   @Override
   public String getDescription() {
     throw new UnsupportedOperationException("Legacy ShadowMatrix APIs are not supported");
+  }
+
+  /** Shadow for {@link Matrix$ExtraNatives} that contains native functions. */
+  @Implements(
+      className = "android.graphics.Matrix$ExtraNatives",
+      isInAndroidSdk = false,
+      callNativeMethodsByDefault = true,
+      shadowPicker = ShadowNativeMatrix.ShadowExtraNatives.Picker.class,
+      minSdk = V.SDK_INT)
+  @SuppressWarnings("robolectric.internal.IgnoreMissingClass")
+  public static class ShadowExtraNatives {
+    /** Shadow picker for {@link Matrix.ExtraNatives}. */
+    public static final class Picker extends GraphicsShadowPicker<Object> {
+      public Picker() {
+        super(null, ShadowNativeMatrix.ShadowExtraNatives.class);
+      }
+    }
   }
 }
