cf660a192b6b297c9d176a3b3790ce7b218e35e2
Rex Hoffman <rexhoffman@google.com>
2024-12-17 17:58:21 +0000

    Allow for RunListeners to be attached via ServiceLoader once per invocation of test
    Bug: 371194831
    Test: local debugging
    Flag: TEST_ONLY
    Change-Id: I527ce6edbf90dd72aede6a6860874931148b5aa8

--- robolectric/src/main/java/org/robolectric/RobolectricTestRunner.java
+++ robolectric/src/main/java/org/robolectric/RobolectricTestRunner.java
@@ -2,6 +2,7 @@ package org.robolectric;
 
 import com.google.auto.service.AutoService;
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import java.io.IOException;
 import java.io.InputStream;
@@ -69,6 +70,8 @@ public class RobolectricTestRunner extends SandboxTestRunner {
   private static final Injector DEFAULT_INJECTOR = defaultInjector().build();
   private static final Map<ManifestIdentifier, AndroidManifest> appManifestsCache = new HashMap<>();
 
+  private static final ImmutableList<RunListener> RUN_LISTENERS = loadRunListeners();
+
   static {
     // This starts up the Poller SunPKCS11-Darwin thread early, outside of any Robolectric
     // classloader.
@@ -79,6 +82,22 @@ public class RobolectricTestRunner extends SandboxTestRunner {
     // validation introduced in Bouncy Castle 1.71.
     // https://github.com/bcgit/bc-java/issues/1144
     System.setProperty("org.bouncycastle.rsa.max_mr_tests", "0");
+  }
+
+  protected static ImmutableList<RunListener> loadRunListeners() {
+    ServiceLoader<RunListener> sl =
+            ServiceLoader.load(RunListener.class, Thread.currentThread().getContextClassLoader());
+    List<RunListener> runListeners = sl.stream().map(ServiceLoader.Provider::get).toList();
+    for (RunListener listener : runListeners) {
+      if (!listener.getClass().getPackageName().startsWith("org.robolectric")) {
+        Logger.warn(
+                "Adding a non-robolectric maintained RunListener"
+                        + " (via Plugins/ServiceLoader) can lead to instability, use at your own risk.\n"
+                        + "Listener is question : "
+                        + listener.getClass().getName());
+      }
+    }
+    return ImmutableList.copyOf(runListeners);
   }
 
   protected static Injector.Builder defaultInjector() {
@@ -120,16 +139,7 @@ public class RobolectricTestRunner extends SandboxTestRunner {
 
   @Override
   public void run(RunNotifier notifier) {
-    ServiceLoader<RunListener> sl =
-        ServiceLoader.load(RunListener.class, Thread.currentThread().getContextClassLoader());
-    for (RunListener listener : sl) {
-      if (!listener.getClass().getPackageName().startsWith("org.robolectric")) {
-        Logger.warn(
-            "Adding a non-robolectric maintained RunListener"
-                + " (via Plugins/ServiceLoader) can lead to instability, use at your own risk.\n"
-                + "Listener in question : "
-                + listener.getClass().getName());
-      }
+    for (RunListener listener : RobolectricTestRunner.RUN_LISTENERS) {
       notifier.addListener(listener);
     }
     super.run(notifier);
